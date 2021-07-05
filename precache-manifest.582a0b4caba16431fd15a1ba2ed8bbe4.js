2e12e702cff50eec13af4501e31046d7b7489e04
root <root@UrUtgUQorz.Some-VPS-name.local>
2021-07-05 10:35:28 +0000

    Initial commit.

---
+++ precache-manifest.582a0b4caba16431fd15a1ba2ed8bbe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc11b46b66ea2d160b48003d6fbde29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e749f420dbd0878339e",
+    "url": "/static/js/2.45916fa2.chunk.js"
+  },
+  {
+    "revision": "7d9a1e97b63fb04c3f7226116d9275de",
+    "url": "/static/js/2.45916fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531b332365be30a4b097",
+    "url": "/static/js/main.2c1d23cb.chunk.js"
+  },
+  {
+    "revision": "7f382bcbaf54759e94e8",
+    "url": "/static/js/runtime-main.7018de35.js"
+  }
+]);
